3151327aaf05fd60ed057d1c6e2745de2d1180d5
Josh Anderson <josh.anderson@dudesolutions.com>
2017-02-27 22:13:49 -0500

    Add image endpoint.

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const mtg = require('mtgsdk')
 var express = require('express')
 var app = express()
 
+app.get('/', function (req, res) {
+    res.send('Having trouble? Check the readme @ https://github.com/nosrednAhsoJ/MTGScryService.')
+})
+
 app.get('/cards/:gathererID', function (req, res) {
     mtg.card.find(req.params['gathererID'])
     .then(result => {
-        res.send(result.card) // "Black Lotus"
+        res.send(result.card)
+    })
+})
+
+app.get('/cards/:gathererID/image', function (req, res) {
+    mtg.card.find(req.params['gathererID'])
+    .then(result => {
+        res.send('<img src="' + result.card.imageUrl + '">')
     })
 })
 
